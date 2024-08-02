a7a99a3fb650d9daabe52f4805be513169e826f5
menn <reinhard.eichhorn@gmail.com>
2024-08-02 03:41:40 +0200

    reafactor(config): Removed path handling from config as it now is managed in `__init__.py` of the package

--- kicker_dyp/config.py
+++ kicker_dyp/config.py
@@ -1,8 +1,4 @@
-import os
-
-
 class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))
     QUALIFYING_FILENAME = 'qualifying-group-1.xml'
     TREE_1_FILENAME = 'elimination-KO-Baum 1.xml'
     TREE_2_FILENAME = 'elimination-KO-Baum 2.xml'
@@ -10,27 +6,21 @@ class Config:
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(Config.basedir, 'dev.db')
     SECRET_KEY = 'your-secret-key'
     SERVER_NAME = 'tablesoccer.rocks:5000'
 
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(Config.basedir, 'test.db')
     SECRET_KEY = 'your-secret-key'
     SERVER_NAME = 'tablesoccer.rocks:5000'
 
 
 class ProdConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(Config.basedir, 'prod.db')
     SERVER_NAME = 'tablesoccer.rocks'
-    # secret key should come from .env file which is obviously not part of the repository
-    # create the file to make it work (compare readme.md)
+    # secret key should come from .env file, create the file to make it work (compare readme.md)
+    # SECRET_KEY = 'your-secret-key'
 
 
 config = {
